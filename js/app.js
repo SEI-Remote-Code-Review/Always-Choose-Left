f6b9f2b8fac7adf097df6df0021b184b91bd6f50
will <wbrycejohnson031086@gmail.com>
2022-02-13 19:41:23 -0500

    add show-scene function and add show game-scenes constant to game-scenes file

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ let inventory = {}
 
 /*------------------------ Cached Element References ------------------------*/
 const text = document.getElementById('text')
-const option = document.getElementById('option-button')
+const optionBtn = document.getElementById('option-button')
 
 /*----------------------------- Event Listeners -----------------------------*/
 
@@ -17,6 +17,14 @@ function init(){
   showGameScene()
 }
 
-function showGameScene(){
-  
+function showGameScene(gameSceneIdx){
+  const gameScene = gameScenes.find(gameScene => gameScene.id === gameSceneIdx)
+  text.innerText = gameScene.text
+  while(optionBtn.firstChild){
+    optionBtn.removeChild(optionBtn.firstChild)
+  }
+}
+
+function showOption(option){
+  return option.requiredInventory === null || option.requiredInventory(inventory)
 }
