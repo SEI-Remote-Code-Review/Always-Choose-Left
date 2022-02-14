8a304ba70a8989fb1b2b87d0e968cfa2c3f8f9f1
will <wbrycejohnson031086@gmail.com>
2022-02-13 19:29:38 -0500

    add cached element references

--- js/app.js
+++ js/app.js
@@ -4,6 +4,8 @@
 let state = {}
 
 /*------------------------ Cached Element References ------------------------*/
+const text = document.getElementById('text')
+const option = document.getElementById('option-button')
 
 /*----------------------------- Event Listeners -----------------------------*/
 
