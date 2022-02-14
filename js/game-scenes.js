33c4751546491b7efccd500f3c661c7652f2fef6
will <wbrycejohnson031086@gmail.com>
2022-02-13 19:44:35 -0500

    add scene id: 1 to game scenes file

--- js/game-scenes.js
+++ js/game-scenes.js
@@ -1,5 +1,17 @@
 const gameScenes = [
   {
-    
-  }
+    id: 1,
+    text: 'As you walk down the overgrown path you can’t believe you let your friends talk you into this. You think to yourself, this place looks like it’s falling down, you don’t care if it’s haunted you don’t really want to go in there, but you’re a teenager and you can’t have your friends thinking you’re scared. As you step onto the porch you hear a noise behind you. You turn slightly. The entrance is now to your left the way back to your right.',
+    options: [
+      {
+        text: 'Go in.',
+        nextText: 2
+      },
+      {
+        text: 'Turn around and run away (screaming like crazy if applicable).',
+        nextText: 1.1
+      }
+    ]
+  },
+  
 ]
